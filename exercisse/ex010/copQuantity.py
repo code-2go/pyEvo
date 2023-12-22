0af4c0941aec1365adc76c7ec6afa8e9f9bc82b2
code-2go <bgurgelamaral@gmail.com>
2023-12-22 19:01:48 -0300

    Logic Repair

--- exercisse/ex010/copQuantity.py
+++ exercisse/ex010/copQuantity.py
@@ -2,5 +2,5 @@ width = float(input('Enter the wall width (in centimeter): '))
 height = float(input('Enter the wall height (in centimeter): '))
 wConversor = width/100
 hConversor = height/100
-# 1 = wconversor = 2 + hConversor = 2
-print('For a {}m x {}m wall, you need {} can of paint to paint all of the wall.'.format(wConversor, hConversor, width/height))
+area = wConversor * hConversor
+print('For a {}m x {}m wall, you need {} can of paint to paint all of the wall.'.format(wConversor, hConversor, area/2))
